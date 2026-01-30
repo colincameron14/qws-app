1b3b7c998eaf03270736b1b651fe6e7457c347ba
colincameron14 <colincameron14@gmail.com>
2026-01-30 12:04:39 -0600

    Add files via upload

---
+++ qws-simple/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{background:'#0a0a0a',minHeight:'100vh',display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'column',color:'#f5f3ef',fontFamily:'Georgia,serif'}}>
+      <h1 style={{color:'#5C6B4A',marginBottom:16}}>Quinn Wildlife Sanctuary</h1>
+      <p>Conservation Mapping System</p>
+      <p style={{marginTop:20,color:'#888'}}>Build successful - full app loading...</p>
+    </div>
+  );
+}
+
+export default App;
